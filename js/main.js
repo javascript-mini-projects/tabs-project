a84b7cac2a3e9619e8a4faf96b0cd2ea25d4aa1e
Harpreet Singh <designsbyharp@gmail.com>
2020-07-06 09:13:56 -0400

    js done

---
+++ js/main.js
@@ -0,0 +1,21 @@
+// Start writing JavaScript here!
+
+const tabs = Array.from(document.querySelectorAll('.jsBtn')),
+      tabby = document.querySelector('.tabby'),
+      tabContents = Array.from(tabby.querySelectorAll('.tab-content'));
+
+tabs.forEach(tab => {
+    tab.addEventListener("click", ()=> {
+        const target = tab.dataset.theme
+        const tabContent = tabby.querySelector("#" + target)
+        console.log(tabContent);
+
+        tabs.forEach(t => t.classList.remove('is-selected'));
+        tab.classList.add('is-selected');
+
+        tabContents.forEach(c => c.classList.remove("is-selected"));
+        tabContent.classList.add('is-selected');
+
+    })
+
+})
